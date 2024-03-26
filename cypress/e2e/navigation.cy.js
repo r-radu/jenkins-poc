30f3210aa5809b3d9a351b535827c8bfc8b97985
r-radu <radur@ensemble.com>
2024-03-26 11:23:10 +0200

    fail test

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -2,5 +2,6 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
     cy.get('[data-testid="shop"]').should('be.visible')
+    cy.get('[data-testid="admin"]').should('be.visible')
   })
 })
